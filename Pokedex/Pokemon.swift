25f15eebeb70c1f0232e955693015ac658c78ad0
RadenAR <radenabdulrahman@gmail.com>
2020-04-13 15:13:39 -0400

    Initial pokedex page

---
+++ Pokedex/Pokemon.swift
@@ -0,0 +1,14 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Raden Abdul Rahman on 4/13/20.
+//  Copyright © 2020 Raden. All rights reserved.
+//
+
+import Foundation
+
+struct Pokemon {
+    let name: String
+    let number: Int
+}
---
+++ Pokedex/PokemonViewController.swift
@@ -0,0 +1,9 @@
+//
+//  PokemonViewController.swift
+//  Pokedex
+//
+//  Created by Raden Abdul Rahman on 4/13/20.
+//  Copyright © 2020 Raden. All rights reserved.
+//
+
+import Foundation
