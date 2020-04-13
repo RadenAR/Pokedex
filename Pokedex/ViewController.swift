710b3e305e31e629ad47d372330cd9731d63ab80
RadenAR <radenabdulrahman@gmail.com>
2020-04-13 14:36:57 -0400

    Initial Commit

---
+++ Pokedex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Raden Abdul Rahman on 4/13/20.
+//  Copyright © 2020 Raden. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
